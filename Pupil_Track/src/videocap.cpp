90ee8b76a6abbf37cf374acfeb53c22d99a4234a
suyash023 <suyashyeotikar@gmail.com>
2014-12-17 14:23:39 +0530

    indemted and formatted

--- Pupil_Track/src/videocap.cpp
+++ Pupil_Track/src/videocap.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <stdio.h>
-#include <opencv2/opencv.hpp>
+#include <opencv2/opencv.hpp> //OpenCV libraries
 #include <opencv/cv.h>
 #include <time.h>
 
@@ -10,12 +10,11 @@ using namespace cv;
 
 char key;
 int main()
-
 {
 int x;
 int y;
-double area;
-int redmax=80;
+double area; //area of contour
+int redmax=80; //thresholds for each channel 
 int redmin=35;
 int greenmax=80;
 int greenmin=35;
@@ -32,59 +31,60 @@ size.height = frameH;
 VideoWriter video("Video.avi",-1,fps,size);
 
 namedWindow("Camera_Output", 1); //Create window
-VideoCapture capture(0); //Capture using any camera connected to your system
-float t0=clock();
+VideoCapture capture(1); //Capture using any camera connected to your system
+float t0=clock(); //measuring time
 float t1=0;
-Mat image;
+Mat image; //input image matrix
 float radius=0;
-while(1){ //Create infinte loop for live streaming
-capture>>image; //Create image frames from capture
-Mat output = Mat::zeros(image.size(),CV_8UC1);
-std::vector<std::vector<cv::Point> > contours;
-//Thresholding the RGB image and converting into binary image
+while(1)
+{ //Create infinte loop for live streaming
+	capture>>image; //Create image frames from capture
+	Mat output = Mat::zeros(image.size(),CV_8UC1);
+	std::vector<std::vector<cv::Point> > contours;
+	//Thresholding the RGB image and converting into binary image
 
-for(y=0;y<image.rows;y++)
-{
-for(x=0;x<image.cols;x++)
-{
-if(((int)image.at<Vec3b>(y,x)[0]>0)&&((int)image.at<Vec3b>(y,x)[0]<20)&&((int)image.at<Vec3b>(y,x)[1]>0)&&((int)image.at<Vec3b>(y,x)[1]<20)&&((int)image.at<Vec3b>(y,x)[2]>0)&&((int)image.at<Vec3b>(y,x)[2]<20))
-{
-output.at<uchar>(y,x)=255;
-}
-}
-}
+	for(y=0;y<image.rows;y++)
+	{
+		for(x=0;x<image.cols;x++)
+		{
+			if(((int)image.at<Vec3b>(y,x)[0]>0)&&((int)image.at<Vec3b>(y,x)[0]<20)&&((int)image.at<Vec3b>(y,x)[1]>0)&&((int)image.at<Vec3b>(y,x)[1]<20)&&((int)image.at<Vec3b>(y,x)[2]>0)&&((int)image.at<Vec3b>(y,x)[2]<20))
+			{
+				output.at<uchar>(y,x)=255;
+			}
+		}
+	}
 
 //Dilation
-Mat element=getStructuringElement( MORPH_RECT,Size(9,9),Point (5,5));
-dilate (output, output, element);
+Mat element=getStructuringElement( MORPH_RECT,Size(9,9),Point (5,5)); //defining structuring element for direction
+dilate (output, output, element); 
 
 //Contour Detection
-findContours(output.clone(), contours, RETR_EXTERNAL, CHAIN_APPROX_NONE);
-cv::drawContours(output, contours, -1, Scalar(255,255,255), -1);
-for (int i = 0; i < contours.size(); i++)
-{
-     area = cv::contourArea(contours[i]);  
-    cv::Rect rect = cv::boundingRect(contours[i]); 
-     radius = rect.width/2;                     
-    if (area >= 400 && std::abs(1 - ((double)rect.width / (double)rect.height)) <= 0.15)
+findContours(output.clone(), contours, RETR_EXTERNAL, CHAIN_APPROX_NONE); //find contours
+cv::drawContours(output, contours, -1, Scalar(255,255,255), -1); //detecting contours
+	for (int i = 0; i < contours.size(); i++)
+	{
+    area = cv::contourArea(contours[i]);    //area of contours
+    cv::Rect rect = cv::boundingRect(contours[i]);  //creating bounding box for contour
+    radius = rect.width/2;                     //radius of circle
+    	if (area >= 400 && std::abs(1 - ((double)rect.width / (double)rect.height)) <= 0.15)
         //std::abs(1 - (area / (CV_PI * std::pow(radius, 2)))) <= 0.2)   
-    {
-        cv::circle(image, cv::Point(rect.x + radius, rect.y + radius), radius, Scalar(0,0,255), 2);
-    }
-}
-
-namedWindow("Input",2);
-imshow("Input",output);
+    	{ //selecting contours based on area and height-width ratio of bounding box
+        cv::circle(image, cv::Point(rect.x + radius, rect.y + radius), radius, Scalar(0,0,255), 2); //impose circle of the found radius over the contour
+    	}
+	}
+namedWindow("Input",2); 
+imshow("Input",image); //BGR image
 namedWindow("output",2);
-imshow("Output",image);  //Show image frames on created window
-t1=clock()-t0;
+imshow("output",output);  //Show image frames on created window
+t1=clock()-t0; //measuring time
 cout<<t1/100000;
 cout<<"\n";
 video.write(output);
 key = waitKey(10); //Capture Keyboard stroke
-if (char(key) == 27){
-break; //If you hit ESC key loop will break.
-}
+	if (char(key) == 27)
+	{
+		break; //If you hit ESC key loop will break.
+	}
 }
 
 return 0;
